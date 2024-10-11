01744e4665bd145b3555a8526b406337d81f6505
CODEsSAMI <samiul.ahmed.home@gmail.com>
2024-10-12 01:37:33 +0600

    hide unhide of sections

--- JS/functions.js
+++ JS/functions.js
@@ -1,40 +1,41 @@
 // cards calculation
-function cardCalculation(inputID, outputID){
-    let  inputMoney = parseInt(document.getElementById(inputID).value);
+function cardCalculation(inputID, outputID) {
+    let inputMoney = parseInt(document.getElementById(inputID).value);
 
     let currentAmount = parseInt(document.getElementById("current-amount").innerText);
 
     let outputMoney = parseInt(document.getElementById(outputID).innerText);
 
-    if(inputMoney <= currentAmount && inputMoney != 0){
+    if (inputMoney <= currentAmount && inputMoney != 0) {
         outputMoney += inputMoney;
 
         return outputMoney;
     }
-    else{
+    else {
         alert("invalid input");
     }
 }
 
 // left amount calculation
-function leftMoney(currentMoneyID, inputID){
-    let  inputMoney = parseInt(document.getElementById(inputID).value);
-    
+function leftMoney(currentMoneyID, inputID) {
+    let inputMoney = parseInt(document.getElementById(inputID).value);
+
     let currentAmount = parseInt(document.getElementById(currentMoneyID).innerText);
 
-    if(inputMoney <= currentAmount && inputMoney != 0){
+    if (inputMoney <= currentAmount && inputMoney != 0) {
         currentAmount -= inputMoney;
         return currentAmount;
     }
-    else{
+    else {
         return currentAmount;
     }
 }
 
 // section view
-function showSection(sectionID){
+function showSection(sectionID) {
     document.getElementById("card-section").classList.add("hidden");
     document.getElementById("history-section").classList.add("hidden");
-    
+
     document.getElementById(sectionID).classList.remove("hidden");
+
 }
--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,42 @@
 // noakhali card
-document.getElementById("noakhali-button").addEventListener("click", function (event){
+document.getElementById("noakhali-button").addEventListener("click", function (event) {
     event.preventDefault;
 
     document.getElementById("noakhali-output").innerText = cardCalculation("noakhali-input", "noakhali-output");
 
     document.getElementById("current-amount").innerText = leftMoney("current-amount", "noakhali-input");
-    
+
 })
 
 // feni card
-document.getElementById("feni-button").addEventListener("click", function (event){
+document.getElementById("feni-button").addEventListener("click", function (event) {
     event.preventDefault;
 
     document.getElementById("feni-output").innerText = cardCalculation("feni-input", "feni-output");
 
     document.getElementById("current-amount").innerText = leftMoney("current-amount", "feni-input");
-    
+
 })
 
 // quota card
-document.getElementById("quota-button").addEventListener("click", function (event){
+document.getElementById("quota-button").addEventListener("click", function (event) {
     event.preventDefault;
 
     document.getElementById("quota-output").innerText = cardCalculation("quota-input", "quota-output");
 
     document.getElementById("current-amount").innerText = leftMoney("current-amount", "quota-input");
-    
+
 })
 
 // donation button 
-document.getElementById("donation-button").addEventListener("click", function (){
+document.getElementById("donation-button").addEventListener("click", function () {
     showSection("card-section");
+
 })
 
 // history button
-document.getElementById("history-button").addEventListener("click", function (){
+document.getElementById("history-button").addEventListener("click", function () {
     showSection("history-section");
+
 })
 
